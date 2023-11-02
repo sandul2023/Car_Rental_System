c073cfee4c8ad505e16703a6857ddc057fcc847d
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:16:31 +0530

    user creden..update

--- Back_End/src/main/java/lk/ijse/carRent/dto/UserCredentialsDTO.java
+++ Back_End/src/main/java/lk/ijse/carRent/dto/UserCredentialsDTO.java
@@ -1,4 +1,16 @@
 package lk.ijse.carRent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class UserCredentialsDTO {
+    private String username;
+    private String password;
+    private String role;
 }
