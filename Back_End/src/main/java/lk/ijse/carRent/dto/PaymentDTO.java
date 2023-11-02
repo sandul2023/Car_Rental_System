1a1d8ef4915cfb296a53b31994c94d5c6831bb80
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:12:23 +0530

    payment updated

--- Back_End/src/main/java/lk/ijse/carRent/dto/PaymentDTO.java
+++ Back_End/src/main/java/lk/ijse/carRent/dto/PaymentDTO.java
@@ -1,4 +1,24 @@
 package lk.ijse.carRent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class PaymentDTO {
+    private String paymentId;
+    private String paymentType;
+    private LocalDate paymentDate;
+    private LocalTime paymentTime;
+    private double amount;
+    private double cash;
+    private double balance;
+    private RentDTO rent;
 }
