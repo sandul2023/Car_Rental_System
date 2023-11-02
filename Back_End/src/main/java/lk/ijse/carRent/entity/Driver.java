bc803ab450884eb1265748157d73ae83d5b15829
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 02:22:18 +0530

    driver

--- Back_End/src/main/java/lk/ijse/carRent/entity/Driver.java
+++ Back_End/src/main/java/lk/ijse/carRent/entity/Driver.java
@@ -1,4 +1,31 @@
 package lk.ijse.carRent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Driver {
+    @Id
+    private String driverId;
+    private String name;
+    private String address;
+    private String contactNumber;
+    private String nic;
+    private String licenseNo;
+    private String licenseImage;
+    private String availabilityType;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private UserCredentials userCredentials;
 }
