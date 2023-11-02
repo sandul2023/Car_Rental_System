484c3052c2af19e62e7f6c6d0890b1f819be0dff
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 02:33:43 +0530

    imageDto

---
+++ Back_End/src/main/java/lk/ijse/carRent/dto/ImageDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.carRent.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class ImageDTO {
+    private String front;
+    private String back;
+    private String side;
+    private String interior;
+}
