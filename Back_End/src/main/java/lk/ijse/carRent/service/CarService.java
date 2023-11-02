dc35770ada2d032118c71fd2615f882c54f99209
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 04:55:29 +0530

    car service

--- Back_End/src/main/java/lk/ijse/carRent/service/CarService.java
+++ Back_End/src/main/java/lk/ijse/carRent/service/CarService.java
@@ -1,4 +1,23 @@
 package lk.ijse.carRent.service;
 
+import lk.ijse.carRent.dto.CarDTO;
+import lk.ijse.carRent.dto.ImageDTO;
+
+import java.util.ArrayList;
+
 public interface CarService {
+    void saveCar(CarDTO carDTO);
+    void updateCar(CarDTO carDTO);
+    void deleteCar(String carId);
+    ArrayList<CarDTO> getAllCars();
+    ArrayList<CarDTO> getAllCarsSortFromBrand();
+    ArrayList<CarDTO> getAllCarsFromCarType(String carType);
+    void uploadCarImages(String carId, String frontImage, String backImage, String sideImage, String interiorImage);
+    ImageDTO getCarImages(String carId);
+    Long getCarCountByCarBrandAndAvailability(String carBrand, String availabilityType);
+    CarDTO getCarFromCarId(String carID);
+    int getAvailableCarCount();
+    int getReservedCarCount();
+    int getNeedToMaintenanceCarCount();
+    int getUnderMaintenanceCarCount();
 }
