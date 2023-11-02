addc1ccb963a927b1740308be0acfae9b5e8908f
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 05:14:10 +0530

    AppWide update

--- Back_End/src/main/java/lk/ijse/carRent/advisor/AppWideExceptionHandler.java
+++ Back_End/src/main/java/lk/ijse/carRent/advisor/AppWideExceptionHandler.java
@@ -1,5 +1,9 @@
 package lk.ijse.carRent.advisor;
 
+import akka.http.scaladsl.model.HttpResponse;
+import com.ubirch.util.http.response.ResponseUtil;
+import com.ubirch.util.model.JsonErrorResponse;
+import com.ubirch.util.model.JsonResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,11 +12,41 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @CrossOrigin
-public class AppWideExceptionHandler<ResponseUtil> {
+public class AppWideExceptionHandler{
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({RuntimeException.class})
     public ResponseUtil handleRuntimeExceptions(RuntimeException e){
         System.out.println(e.getMessage());
-        return new ResponseUtil("Error", e.getMessage(), null);
+        return new ResponseUtil() {
+            @Override
+            public HttpResponse response(String s) {
+                return null;
+            }
+
+            @Override
+            public HttpResponse response(JsonResponse jsonResponse) {
+                return null;
+            }
+
+            @Override
+            public HttpResponse requestErrorResponse(String s, String s1) {
+                return null;
+            }
+
+            @Override
+            public HttpResponse requestErrorResponse(JsonErrorResponse jsonErrorResponse) {
+                return null;
+            }
+
+            @Override
+            public HttpResponse serverErrorResponse(String s, String s1) {
+                return null;
+            }
+
+            @Override
+            public HttpResponse serverErrorResponse(JsonErrorResponse jsonErrorResponse) {
+                return null;
+            }
+        };
     }
 }
