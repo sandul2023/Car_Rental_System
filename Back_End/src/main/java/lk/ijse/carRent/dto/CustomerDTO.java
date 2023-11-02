90aa05b6be5e5440980e2dc5303681c4d8c6e1ef
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:08:54 +0530

    customer Dto update

--- Back_End/src/main/java/lk/ijse/carRent/dto/CustomerDTO.java
+++ Back_End/src/main/java/lk/ijse/carRent/dto/CustomerDTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.carRent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class CustomerDTO {
+    private String customerId;
+    private String nic;
+    private String name;
+    private String email;
+    private String address;
+    private String contactNumber;
+    private String licenceNo;
+    private UserCredentialsDTO userCredentials;
 }
