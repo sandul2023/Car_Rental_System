a02e98c301cc8127a357d68a38e38588b444be3e
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:10:46 +0530

    driver Dto update

--- Back_End/src/main/java/lk/ijse/carRent/dto/DriverDTO.java
+++ Back_End/src/main/java/lk/ijse/carRent/dto/DriverDTO.java
@@ -1,4 +1,22 @@
 package lk.ijse.carRent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class DriverDTO {
+    private String driverId;
+    private String name;
+    private String address;
+    private String contactNumber;
+    private String nic;
+    private String licenseNo;
+    private String licenseImage;
+    private String availabilityType;
+    private UserCredentialsDTO userCredentials;
 }
