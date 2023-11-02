22befd0bdbe3329ce568054abc666149ffdd9c65
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:49:42 +0530

    customer image

--- Back_End/src/main/java/lk/ijse/carRent/embedded/CustomerImage.java
+++ Back_End/src/main/java/lk/ijse/carRent/embedded/CustomerImage.java
@@ -1,4 +1,16 @@
 package lk.ijse.carRent.embedded;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embeddable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Embeddable
 public class CustomerImage {
+    private String nicImage;
+    private String licenseImage;
 }
