92d57e051af30cee2416a21dc60d0bec0f8c5a26
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 02:11:53 +0530

    user

--- Back_End/src/main/java/lk/ijse/carRent/entity/Admin.java
+++ Back_End/src/main/java/lk/ijse/carRent/entity/Admin.java
@@ -1,4 +1,27 @@
 package lk.ijse.carRent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Admin {
+    @Id
+    private String adminNic;
+    private String adminName;
+    private String contactNumber;
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private UserCredentials userCredentials;
 }
--- Back_End/src/main/java/lk/ijse/carRent/entity/UserCredentials.java
+++ Back_End/src/main/java/lk/ijse/carRent/entity/UserCredentials.java
@@ -1,4 +1,21 @@
 package lk.ijse.carRent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class UserCredentials {
+    @Id
+    private String username;
+    private String password;
+    private String role;
 }
