64a08d31148e9596044ade631fb82371fe560421
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:07:08 +0530

    image update

--- Back_End/src/main/java/lk/ijse/carRent/dto/CarImageDTO.java
+++ Back_End/src/main/java/lk/ijse/carRent/dto/CarImageDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.carRent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class CarImageDTO {
+    private MultipartFile front;
+    private MultipartFile back;
+    private MultipartFile side;
+    private MultipartFile interior;
 }
