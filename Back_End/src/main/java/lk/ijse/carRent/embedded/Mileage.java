580e7f8b9e92a562705e7472d46cc20172b0b47b
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:52:51 +0530

    price rate

--- Back_End/src/main/java/lk/ijse/carRent/embedded/Mileage.java
+++ Back_End/src/main/java/lk/ijse/carRent/embedded/Mileage.java
@@ -1,4 +1,18 @@
 package lk.ijse.carRent.embedded;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embeddable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Embeddable
 public class Mileage {
+    private int dailyMileage;
+    private int monthlyMileage;
 }
