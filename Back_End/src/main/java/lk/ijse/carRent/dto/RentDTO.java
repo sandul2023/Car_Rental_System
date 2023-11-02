420c3b4bae31096a18a8d03827c160d3ddaca839
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:15:13 +0530

    rent update

--- Back_End/src/main/java/lk/ijse/carRent/dto/RentDTO.java
+++ Back_End/src/main/java/lk/ijse/carRent/dto/RentDTO.java
@@ -1,4 +1,28 @@
 package lk.ijse.carRent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class RentDTO {
+    private String rentId;
+    private LocalTime pickUpTime;
+    private LocalDate pickUpDate;
+    private LocalTime returnTime;
+    private LocalDate returnDate;
+    private String requestTypeOfDriver;
+    private String location;
+    private String rentStatus;
+    private String deniedReason;
+    private CustomerDTO customer;
+    private List<RentDetailDTO> rentDetail;
 }
