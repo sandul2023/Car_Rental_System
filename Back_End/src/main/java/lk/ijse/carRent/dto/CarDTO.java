3c8dc06fb0a6649506e10b95a2080092171db747
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:05:23 +0530

    car update

--- Back_End/src/main/java/lk/ijse/carRent/dto/CarDTO.java
+++ Back_End/src/main/java/lk/ijse/carRent/dto/CarDTO.java
@@ -1,4 +1,27 @@
 package lk.ijse.carRent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class CarDTO {
+    private String carId;
+    private String registerNum;
+    private String brand;
+    private String type;
+    private PriceRate priceRate;
+    private Mileage freeMileage;
+    private String color;
+    private String transmissionType;
+    private int numOfPassengers;
+    private String fuelType;
+    private double pricePerExtraKM;
+    private double lossDamageWaiver;
+    private double lastServiceMileage;
+    private CarImage availabilityType;
 }
