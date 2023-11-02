e322e919c6963d3a7792341e70c8c055e8e8703e
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-02 22:14:08 +0530

    rent details update

--- Back_End/src/main/java/lk/ijse/carRent/dto/RentDetailDTO.java
+++ Back_End/src/main/java/lk/ijse/carRent/dto/RentDetailDTO.java
@@ -1,4 +1,20 @@
 package lk.ijse.carRent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class RentDetailDTO {
+    private String rentId;
+    private String carId;
+    private double distanceMileage;
+    private double carCost;
+    private double driverCost;
+    private double damageFee;
+    private DriverDTO driver;
 }
