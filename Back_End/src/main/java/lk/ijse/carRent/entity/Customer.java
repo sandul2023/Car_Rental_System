36583bbafaba4894e487dabd4b903636b8e4803f
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 02:19:47 +0530

    customer

--- Back_End/src/main/java/lk/ijse/carRent/entity/Customer.java
+++ Back_End/src/main/java/lk/ijse/carRent/entity/Customer.java
@@ -1,4 +1,31 @@
 package lk.ijse.carRent.entity;
 
+import lk.ijse.carRent.embedded.CustomerImage;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Customer {
+    @Id
+    private String customerId;
+    private String nic;
+    private String name;
+    private String email;
+    private String address;
+    private String contactNumber;
+    private String licenceNo;
+
+    @Embedded
+    private CustomerImage uploadImages;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private UserCredentials userCredentials;
 }
