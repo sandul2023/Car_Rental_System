aa253f7e1e080f859023f6b3b27acf2622c1245f
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 02:28:51 +0530

    payment & rent

--- Back_End/src/main/java/lk/ijse/carRent/entity/Payment.java
+++ Back_End/src/main/java/lk/ijse/carRent/entity/Payment.java
@@ -1,4 +1,30 @@
 package lk.ijse.carRent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Payment {
+    @Id
+    private String paymentId;
+    private String paymentType;
+    private LocalDate paymentDate;
+    private LocalTime paymentTime;
+    private double amount;
+    private double cash;
+    private double balance;
+
+    @ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "rentId", referencedColumnName = "rentId", nullable = false)
+    private Rent rent;
 }
--- Back_End/src/main/java/lk/ijse/carRent/entity/Rent.java
+++ Back_End/src/main/java/lk/ijse/carRent/entity/Rent.java
@@ -1,4 +1,36 @@
 package lk.ijse.carRent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Rent {
+    @Id
+    private String rentId;
+    private LocalTime pickUpTime;
+    private LocalDate pickUpDate;
+    private LocalTime returnTime;
+    private LocalDate returnDate;
+    private String requestTypeOfDriver;
+    private String location;
+    private String rentStatus;
+    private String deniedReason;
+
+    @ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "customerId", referencedColumnName = "customerId", nullable = false)
+    private Customer customer;
+
+    @OneToMany(mappedBy = "rent",cascade = CascadeType.ALL)
+    private List<RentDetail> rentDetails;
 }
