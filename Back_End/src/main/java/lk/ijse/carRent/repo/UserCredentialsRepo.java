510154293a4aa77808c76d3d94220a92844b624a
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 04:47:43 +0530

    repo update

--- Back_End/src/main/java/lk/ijse/carRent/repo/UserCredentialsRepo.java
+++ Back_End/src/main/java/lk/ijse/carRent/repo/UserCredentialsRepo.java
@@ -1,4 +1,10 @@
 package lk.ijse.carRent.repo;
 
-public interface UserCredentialsRepo {
+import lk.ijse.carRent.entity.UserCredentials;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCredentialsRepo extends JpaRepository<UserCredentials, String> {
+    Boolean existsByUsername(String username);
+    UserCredentials findUserCredentialsByUsername(String username);
+    UserCredentials findUsernameAndPassword(String username, String password);
 }
