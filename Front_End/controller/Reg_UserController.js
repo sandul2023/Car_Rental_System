13c37f1a2894c4211df3af20df21044e514ced9b
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 21:57:27 +0530

    Controller update

--- Front_End/controller/Reg_UserController.js
+++ Front_End/controller/Reg_UserController.js
@@ -1,42 +1,40 @@
 
-let driverBaseUrl = "http://localhost:8080/Back_End_war/";
-loadAllDrivers();
-
-$("#btnSaveDriver").attr('disabled', true);
-$("#btnUpdateDriver").attr('disabled', true);
-$("#btnDeleteDriver").attr('disabled', true);
-
+let userBaseUrl = "http://localhost:8080/Back_End_war/";
+loadAllRegUsers();
+$("#btnSaveCustomer").attr('disabled', true);
+$("#btnUpdateCustomer").attr('disabled', true);
+$("#btnDeleteCustomer").attr('disabled', true);
 
 /**
  * Customer Save
  * */
-$("#btnSaveDriver").click(function () {
-    let formData = new FormData($("#driverForm")[0]);
+$("#btnSaveCustomer").click(function () {
+    let formData = new FormData($("#customerForm")[0]);
     console.log(formData);
     $.ajax({
-        url: driverBaseUrl + "driver",
+        url: userBaseUrl + "reg_User",
         method: "post",
         data: formData,
         contentType: false,
         processData: false,
         success: function (res) {
-            console.log(res)
-            saveUpdateAlert("Driver", res.message);
-            loadAllDrivers();
+            saveUpdateAlert("User", res.message);
+            loadAllRegUsers();
         },
         error: function (error) {
-            unSuccessUpdateAlert("Driver", JSON.parse(error.responseText).message);
+            unSuccessUpdateAlert("User", JSON.parse(error.responseText).message);
         }
     });
 });
 
+
 /**
- * User Driver Generator
+ * User Id Generator
  * */
-function generateDriverID() {
-    $("#user_Id").val("DRI-001");
+function generateCustomerID() {
+    $("#user_Id").val("C00-001");
     $.ajax({
-        url: driverBaseUrl + "driver/driverIdGenerate",
+        url: userBaseUrl + "reg_User/reg_UserIdGenerate",
         method: "GET",
         contentType: "application/json",
         dataType: "json",
@@ -46,11 +44,11 @@ function generateDriverID() {
             let tempId = parseInt(id.split("-")[1]);
             tempId = tempId + 1;
             if (tempId <= 9) {
-                $("#user_Id").val("DRI-00" + tempId);
+                $("#user_Id").val("C00-00" + tempId);
             } else if (tempId <= 99) {
-                $("#user_Id").val("DRI-0" + tempId);
+                $("#user_Id").val("C00-0" + tempId);
             } else {
-                $("#user_Id").val("DRI-" + tempId);
+                $("#user_Id").val("C00-" + tempId);
             }
         },
         error: function (ob, statusText, error) {
@@ -61,31 +59,31 @@ function generateDriverID() {
 /**
  * clear input fields Values Method
  * */
-function setTextFieldValuesD(firstName, lastName, contact_No, address, email, nic_No, license_No, license_Img, driverAvailability, user_Name, password) {
+function setTextFieldValues(firstName, lastName, contact_No, address, email, nic, license_No, nic_Img, license_Img, user_Name, password) {
     $("#firstName").val(firstName);
     $("#lastName").val(lastName);
     $("#contact_No").val(contact_No);
     $("#address").val(address);
     $("#email").val(email);
-    $("#nic_No").val(nic_No);
+    $("#nic").val(nic);
     $("#license_No").val(license_No);
+    $("#nic_Img").val(nic_Img);
     $("#license_Img").val(license_Img);
-    $("#driverAvailability").val(driverAvailability);
     $("#user_Name").val(user_Name);
     $("#password").val(password);
 
     $("#firstName").focus();
-    checkValidity(driverValidations);
-    $("#btnSaveDriver").attr('disabled', true);
+    checkValidity(customerValidations);
+    $("#btnSaveCustomer").attr('disabled', true);
 }
 
 /**
  * load all customers Method
  * */
-function loadAllDrivers() {
-    $("#driverTable").empty();
+function loadAllRegUsers() {
+    $("#customerTable").empty();
     $.ajax({
-        url: driverBaseUrl + "driver/loadAllDrivers", method: "GET", dataType: "json", success: function (res) {
+        url: userBaseUrl + "reg_User/loadAllUsers", method: "GET", dataType: "json", success: function (res) {
             console.log(res);
 
             for (let i of res.data) {
@@ -95,26 +93,25 @@ function loadAllDrivers() {
                 let contact_No = i.contact_No;
                 let address = i.address;
                 let email = i.email;
-                let nic_No = i.nic_No;
+                let nic = i.nic;
                 let license_No = i.license_No;
+                let nic_Img = i.nic_Img;
                 let license_Img = i.license_Img;
-                let driverAvailability = i.driverAvailability;
                 let role_Type = i.user.role_Type;
                 let user_Name = i.user.user_Name;
                 let password = i.user.password;
 
-                let row = "<tr><td>" + user_Id + "</td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + contact_No + "</td><td>" + address + "</td><td>" + email + "</td><td>" + nic_No + "</td><td>" + license_No + "</td><td>" + driverAvailability + "</td><td>" + role_Type + "</td><td>" + user_Name + "</td><td>" + password + "</td></tr>";
-                $("#driverTable").append(row);
+                let row = "<tr><td>" + user_Id + "</td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + contact_No + "</td><td>" + address + "</td><td>" + email + "</td><td>" + nic + "</td><td>" + license_No + "</td><td>" + role_Type + "</td><td>" + user_Name + "</td><td>" + password + "</td></tr>";
+                $("#customerTable").append(row);
             }
-            blindClickEventsD();
-            generateDriverID();
-            setTextFieldValuesD("", "", "", "", "", "", "", "", "", "", "");
+            blindClickEvents();
+            generateCustomerID();
+            setTextFieldValues("", "", "", "", "", "", "", "", "", "", "");
             console.log(res.message);
         }, error: function (error) {
             let message = JSON.parse(error.responseText).message;
             console.log(message);
         }
-
     });
 }
 
@@ -124,9 +121,9 @@ function loadAllDrivers() {
 $("#search_Id").on("keypress", function (event) {
     if (event.which === 13) {
         var search = $("#search_Id").val();
-        $("#driverTable").empty();
+        $("#customerTable").empty();
         $.ajax({
-            url: driverBaseUrl + "driver/searchDriver/?driver_Id="+ search,
+            url: userBaseUrl + "reg_User/searchCustomer/?cus_Id="+ search,
             method: "GET",
             contentType: "application/json",
             dataType: "json",
@@ -138,18 +135,19 @@ $("#search_Id").on("keypress", function (event) {
                 $("#contact_No").val(res.contact_No);
                 $("#address").val(res.address);
                 $("#email").val(res.email);
-                $("#nic_No").val(res.nic_No);
+                $("#nic").val(res.nic);
                 $("#license_No").val(res.license_No);
+                $("#nic_Img").prop(res.nic_Img);
                 $("#license_Img").prop(res.license_Img);
                 $("#driverAvailability").val(res.driverAvailability);
                 $("#role_Type").val(res.user.role_Type);
                 $("#user_Name").val(res.user.user_Name);
                 $("#password").val(res.user.password);
-                let row = "<tr><td>" + res.user_Id + "</td><td>" + res.name.firstName + "</td><td>" + res.name.lastName + "</td><td>" + res.contact_No + "</td><td>" + res.address + "</td><td>" + res.email + "</td><td>" + res.nic_No + "</td><td>" + res.license_No + "</td><td>" + res.driverAvailability + "</td><td>" + res.user.role_Type + "</td><td>" + res.user.user_Name + "</td><td>" + res.user.password + "</td></tr>";
-                $("#driverTable").append(row);
+                let row = "<tr><td>" + res.user_Id + "</td><td>" + res.name.firstName + "</td><td>" + res.name.lastName + "</td><td>" + res.contact_No + "</td><td>" + res.address + "</td><td>" + res.email + "</td><td>" + res.nic + "</td><td>" + res.license_No + "</td><td>" + res.user.role_Type + "</td><td>" + res.user.user_Name + "</td><td>" + res.user.password + "</td></tr>";
+                $("#customerTable").append(row);
             },
             error: function (error) {
-                loadAllDrivers();
+                loadAllRegUsers();
                 let message = JSON.parse(error.responseText).message;
                 emptyMassage(message);
             }
@@ -158,83 +156,79 @@ $("#search_Id").on("keypress", function (event) {
 
 });
 
-
 /**
  * Table Listener Click and Load textFields
  * */
-function blindClickEventsD() {
-    $("#driverTable>tr").on("click", function () {
+function blindClickEvents() {
+    $("#customerTable>tr").on("click", function () {
         let user_Id = $(this).children().eq(0).text();
         let firstName = $(this).children().eq(1).text();
         let lastName = $(this).children().eq(2).text();
-        let contact_No = $(this).children().eq(3).text();
-        let address = $(this).children().eq(4).text();
+        let address = $(this).children().eq(3).text();
+        let contact_No = $(this).children().eq(4).text();
         let email = $(this).children().eq(5).text();
-        let nic_No = $(this).children().eq(6).text();
+        let nic = $(this).children().eq(6).text();
         let license_No = $(this).children().eq(7).text();
-        let driverAvailability = $(this).children().eq(8).text();
-        let role_Type = $(this).children().eq(9).text();
-        let user_Name = $(this).children().eq(10).text();
-        let password = $(this).children().eq(11).text();
+        let role_Type = $(this).children().eq(8).text();
+        let user_Name = $(this).children().eq(9).text();
+        let password = $(this).children().eq(10).text();
 
 
-        console.log(user_Id, firstName, lastName, contact_No, address, email, nic_No, license_No, driverAvailability, role_Type, user_Name,password);
+        console.log(user_Id, firstName, lastName, address, contact_No, email, nic, license_No, role_Type, user_Name, password);
 
         $("#user_Id").val(user_Id);
         $("#firstName").val(firstName);
         $("#lastName").val(lastName);
-        $("#contact_No").val(contact_No);
-        $("#address").val(address);
+        $("#contact_No").val(address);
+        $("#address").val(contact_No);
         $("#email").val(email);
-        $("#nic_No").val(nic_No);
+        $("#nic").val(nic);
         $("#license_No").val(license_No);
-        $("#driverAvailability").val(driverAvailability);
         $("#role_Type").val(role_Type);
         $("#user_Name").val(user_Name);
         $("#password").val(password);
     });
-    $("#btnSaveDriver").attr('disabled', true);
+    $("#btnSaveCustomer").attr('disabled', true);
 }
 
 /**
  * Update Action
  * */
-$("#btnUpdateDriver").click(function () {
-    let formData = new FormData($("#driverForm")[0]);
+$("#btnUpdateCustomer").click(function () {
+    let formData = new FormData($("#customerForm")[0]);
     console.log(formData);
     $.ajax({
-        url: driverBaseUrl + "driver/update",
+        url: userBaseUrl + "reg_User/update",
         method: "post",
         data: formData,
         contentType: false,
         processData: false,
         success: function (res) {
-            console.log(res)
-            saveUpdateAlert("Driver", res.message);
-            loadAllDrivers();
+            saveUpdateAlert("User", res.message);
+            loadAllRegUsers();
         },
         error: function (error) {
-            unSuccessUpdateAlert("Driver", JSON.parse(error.responseText).message);
+            unSuccessUpdateAlert("User", JSON.parse(error.responseText).message);
         }
     });
 });
-
 
 /**
  * Delete Action
  * */
-$("#btnDeleteDriver").click(function () {
+$("#btnDeleteCustomer").click(function () {
     let id = $("#user_Id").val();
     $.ajax({
-        url: driverBaseUrl + "driver?id=" + id , method: "delete", dataType: "json", success: function (resp) {
-            saveUpdateAlert("Driver", resp.message);
-            loadAllDrivers();
+        url: userBaseUrl + "reg_User?id=" + id + "", method: "delete", dataType: "json", success: function (resp) {
+            saveUpdateAlert("User", resp.message);
+            loadAllRegUsers();
         }, error: function (error) {
             let message = JSON.parse(error.responseText).message;
-            unSuccessUpdateAlert("Driver", message);
+            unSuccessUpdateAlert("User", message);
         }
     });
 });
+
 
 /**
  * Auto Forces Input Fields Save
@@ -250,47 +244,47 @@ const regExDrivingNIC = /^[A-Z0-9-]+$/;
 const regExUserName = /^[A-z0-9/ ]{4,30}$/;
 const regExPassword = /^([A-Z a-z]{5,15}[0-9]{1,10})$/;
 
-let driverValidations = [];
-driverValidations.push({
-    reg: regExFirstName, field: $('#firstName'), error: 'Driver First Name Pattern is Wrong'
+let customerValidations = [];
+customerValidations.push({
+    reg: regExFirstName, field: $('#firstName'), error: 'Customer First Name Pattern is Wrong'
 });
-driverValidations.push({
-    reg: regExLastName, field: $('#lastName'), error: 'Driver Last Name Pattern is Wrong'
+customerValidations.push({
+    reg: regExLastName, field: $('#lastName'), error: 'Customer Last Name Pattern is Wrong'
 });
-driverValidations.push({
-    reg: regExContactNum, field: $('#contact_No'), error: 'Driver Contact Number Pattern is Wrong'
+customerValidations.push({
+    reg: regExContactNum, field: $('#contact_No'), error: 'Customer Contact Number Pattern is Wrong'
 });
-driverValidations.push({
-    reg: regExCusAddress, field: $('#address'), error: 'Driver Address Pattern is Wrong'
+customerValidations.push({
+    reg: regExCusAddress, field: $('#address'), error: 'Customer Address Pattern is Wrong'
 });
-driverValidations.push({
-    reg: regExEmailCusAddress, field: $('#email'), error: 'Driver Email Address Pattern is Wrong'
+customerValidations.push({
+    reg: regExEmailCusAddress, field: $('#email'), error: 'Customer Email Address Pattern is Wrong'
 });
-driverValidations.push({
-    reg: regExNIC, field: $('#nic_No'), error: 'Driver NIC Pattern is Wrong'
+customerValidations.push({
+    reg: regExNIC, field: $('#nic'), error: 'Customer NIC Pattern is Wrong'
 });
-driverValidations.push({
-    reg: regExDrivingNIC, field: $('#license_No'), error: 'Driver Driving License Pattern is Wrong'
+customerValidations.push({
+    reg: regExDrivingNIC, field: $('#license_No'), error: 'Customer Driving License Pattern is Wrong'
 });
-driverValidations.push({
-    reg: regExUserName, field: $('#user_Name'), error: 'Driver User Name Pattern is Wrong'
+customerValidations.push({
+    reg: regExUserName, field: $('#user_Name'), error: 'Customer User Name Pattern is Wrong'
 });
-driverValidations.push({
-    reg: regExPassword, field: $('#password'), error: 'Driver Password Pattern is Wrong'
+customerValidations.push({
+    reg: regExPassword, field: $('#password'), error: 'Customer Password Pattern is Wrong'
 });
 //disable tab key of all four text fields using grouping selector in CSS
-$("#firstName,#lastName,#contact_No,#address,#email,#nic_No,#license_No,#user_Name,#password").on('keydown', function (event) {
+$("#firstName,#lastName,#contact_No,#address,#email,#nic,#license_No,#user_Name,#password").on('keydown', function (event) {
     if (event.key === "Tab") {
         event.preventDefault();
     }
 });
 
-$("#firstName,#lastName,#contact_No,#address,#email,#nic_No,#license_No,#user_Name,#password").on('keyup', function (event) {
-    checkValidity(driverValidations);
+$("#firstName,#lastName,#contact_No,#address,#email,#nic,#license_No,#user_Name,#password").on('keyup', function (event) {
+    checkValidity(customerValidations);
 });
 
-$("#firstName,#lastName,#contact_No,#address,#email,#nic_No,#license_No,#user_Name,#password").on('blur', function (event) {
-    checkValidity(driverValidations);
+$("#firstName,#lastName,#contact_No,#address,#email,#nic,#license_No,#user_Name,#password").on('blur', function (event) {
+    checkValidity(customerValidations);
 });
 
 $("#firstName").on('keydown', function (event) {
@@ -323,12 +317,12 @@ $("#address").on('keydown', function (event) {
 
 $("#email").on('keydown', function (event) {
     if (event.key === "Enter" && check(regExEmailCusAddress, $("#email"))) {
-        focusText($("#nic_No"));
+        focusText($("#nic"));
     }
 });
 
-$("#nic_No").on('keydown', function (event) {
-    if (event.key === "Enter" && check(regExNIC, $("#nic_No"))) {
+$("#nic").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExNIC, $("#nic"))) {
         focusText($("#license_No"));
     }
 });
@@ -352,7 +346,7 @@ $("#user_Name").on('keydown', function (event) {
 $("#password").on('keydown', function (event) {
     if (event.key === "Enter" && check(regExPassword, $("#password"))) {
         if (event.which === 13) {
-            $('#btnSaveDriver').focus();
+            $('#btnSaveCustomer').focus();
         }
     }
 });
@@ -360,12 +354,12 @@ $("#password").on('keydown', function (event) {
 
 function setButtonState(value) {
     if (value > 0) {
-        $("#btnSaveDriver").attr('disabled', true);
-        $("#btnUpdateDriver").attr('disabled', true);
-        $("#btnDeleteDriver").attr('disabled', true);
+        $("#btnSaveCustomer").attr('disabled', true);
+        $("#btnUpdateCustomer").attr('disabled', true);
+        $("#btnDeleteCustomer").attr('disabled', true);
     } else {
-        $("#btnSaveDriver").attr('disabled', false);
-        $("#btnUpdateDriver").attr('disabled', false);
-        $("#btnDeleteDriver").attr('disabled', false);
+        $("#btnSaveCustomer").attr('disabled', false);
+        $("#btnUpdateCustomer").attr('disabled', false);
+        $("#btnDeleteCustomer").attr('disabled', false);
     }
 }
