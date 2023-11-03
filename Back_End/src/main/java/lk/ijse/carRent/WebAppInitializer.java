5ef571a32a73edd4c54d3f55897178f414cbfbed
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2023-11-03 16:08:15 +0530

    update

--- Back_End/src/main/java/lk/ijse/carRent/WebAppInitializer.java
+++ Back_End/src/main/java/lk/ijse/carRent/WebAppInitializer.java
@@ -1,20 +1,22 @@
 package lk.ijse.carRent;
 
+import lk.ijse.carRent.config.WebAppConfig;
+import lk.ijse.carRent.config.WebRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
